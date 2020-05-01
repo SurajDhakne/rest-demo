63c42d3206e6241aa9b8d240b97f3e696a0b103a
Suraj01 <Suraj01@Suraj>
2020-05-01 17:58:29 +0530

    second commit

--- demo-project/src/main/java/com/sip/test/service/RestApiService.java
+++ demo-project/src/main/java/com/sip/test/service/RestApiService.java
@@ -1,18 +1,23 @@
 package com.sip.test.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.sip.test.entity.DamageImage;
 import com.sip.test.repository.DamageRepository;
+import com.sip.test.response.DamageApiResponse;
 
 @Service
 public class RestApiService {
 	@Autowired
 	DamageRepository repository;
-	
+	HttpStatus status;
 	public DamageImage addDamageImage(DamageImage dImage) {
+		
+//		DamageImage dm= repository.save(dImage);
+//		DamageApiResponse ff= new DamageApiResponse(dm,null,status.OK,"ok",false);
 		return repository.save(dImage); 
 	}
 
